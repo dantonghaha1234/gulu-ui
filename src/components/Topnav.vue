58ccfe25f92241011f31dfdaf43e738e96de0781
zhaodan <450535034@qq.com>
2020-10-16 10:43:59 +0800

    用provide和inject实现切换功能

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="topnav">
-        <div class="logo">LOGO</div>
+        <div class="logo" @click="toggleMenu">LOGO</div>
         <ul class="menu">
             <li>菜单1</li>
             <li>菜单2</li>
@@ -8,7 +8,16 @@
     </div>
 </template>
 <script lang="ts">
+    import {inject,Ref} from 'vue'
     export default {
+       setup(){
+           const menuVisible = inject<Ref<boolean>>('menuVisible')
+            const toggleMenu = ()=>{
+               menuVisible.value = !menuVisible.value;
+            }
+            console.log({toggleMenu});
+            return {toggleMenu}
+       },
 
     }
 </script>
@@ -17,6 +26,8 @@
         background: pink;
         display: flex;
         padding: 16px;
+        position: relative;
+        z-index: 10;
         > .logo {
             max-width: 6em;
             margin-right: auto;
--- src/view/Doc.vue
+++ src/view/Doc.vue
@@ -2,7 +2,7 @@
     <div>
         <Topnav />
         <div class="content">
-            <aside>
+            <aside v-if="menuVisible">
                 <h2>组件列表</h2>
                 <ol>
                     <li>
@@ -25,8 +25,13 @@
 </template>
 <script lang="ts">
     import Topnav from "../components/Topnav.vue";
+    import {inject, Ref} from 'vue';
     export default {
         components: { Topnav },
+        setup(){
+            const menuVisible = inject<Ref<boolean>>('menuVisible')
+            return {menuVisible};
+        }
     };
 </script>
 <style lang="scss" scoped>
@@ -37,6 +42,7 @@
         position: fixed;
         top: 0;
         left: 0;
+        padding-top: 70px;
         > h2 {
             margin-bottom: 4px;
         }
