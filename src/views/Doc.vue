8220c368b2d5be0a627db3e01160017c359ad98a
zhaodan <450535034@qq.com>
2020-10-19 12:37:47 +0800

    抽离switch组件

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -52,62 +52,60 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.layout {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-
-  >.nav {
-    flex-shrink: 0;
-  }
-
-  >.content {
-    flex-grow: 1;
-    padding-top: 60px;
-    padding-left: 156px;
-
-    @media (max-width: 500px) {
-      padding-left: 0;
+  .layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    >.nav {
+      flex-shrink: 0;
+    }
+    >.content {
+      flex-grow: 1;
+      padding-top: 60px;
+      padding-left: 156px;
+      @media (max-width: 500px) {
+        padding-left: 0;
+      }
     }
   }
-}
-
-.content {
-  display: flex;
-
-  >aside {
-    flex-shrink: 0;
-  }
-
-  >main {
-    flex-grow: 1;
-    padding: 16px;
-    background: white;
-  }
-}
-
-aside {
-  background: lightblue;
-  width: 150px;
-  padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
-  height: 100%;
-
-  >h2 {
-    margin-bottom: 4px;
-  }
-
-  >ol {
-    >li {
-      padding: 4px 0;
+  .content {
+    display: flex;
+    >aside {
+      flex-shrink: 0;
+    }
+    >main {
+      flex-grow: 1;
+      padding: 16px;
+      background: white;
     }
   }
-}
-
-main {
-  overflow: auto;
-}
+  aside {
+    background: lightblue;
+    width: 150px;
+    padding: 16px 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
+    height: 100%;
+    >h2 {
+      margin-bottom: 4px;
+      padding: 0 16px;
+    }
+    >ol {
+      >li {
+        >a {
+          display: block;
+          padding: 4px 16px;
+          text-decoration: none;
+        }
+        .router-link-active {
+          background: white;
+        }
+      }
+    }
+  }
+  main {
+    overflow: auto;
+  }
 </style>
