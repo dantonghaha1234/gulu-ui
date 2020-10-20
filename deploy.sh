9d78f5bf639c05a7654948f779d4e73879710fa9
zhaodan <450535034@qq.com>
2020-10-20 11:18:47 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ git init
 git add .
 git commit -m "update"
 git branch -M master
-git remote add origin git@github.com:dantonghaha1234/gulu-ui.git
+git remote add origin git@github.com:dantonghaha1234/adam-ui-website.git
 git push -f -u origin master
 cd ..
