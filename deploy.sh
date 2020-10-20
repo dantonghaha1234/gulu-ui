ee5fc4c0560cbfad5ae877db46279a3331c312cc
zhaodan <450535034@qq.com>
2020-10-20 11:28:31 +0800

    一件部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
-rm -rf dist
-npm install build
-cd dist
-git init
-git add .
-git commit -m "update"
-git branch -M master
-git remote add origin git@github.com:dantonghaha1234/adam-ui-website.git
-git push -f -u origin master
-cd ..
+rm -rf dist &&
+npm run build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M master &&
+git remote add origin git@github.com:dantonghaha1234/adam-ui-website.git &&
+git push -f -u origin master &&
+cd .. 
+echo https://dantonghaha1234.github.io/adam-ui-website/index.html
